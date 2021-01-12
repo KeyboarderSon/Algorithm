c3496cc633bec07656211063f07f2b8ae191bbe0
KeyboaderSon <juanjh60@naver.com>
2021-01-13 00:29:30 +0900

    재귀 n과 m 시리즈

---
+++ 15650.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int N, M;
+int arr[8];
+int ans[8];
+bool check[8]={false, };
+
+
+void Print(){
+    for(int i=0;i<M;i++){
+        cout<<ans[i];
+    }
+    cout<<"\n";
+}
+
+void deep(int depth, int start){
+    if(depth==M){
+        Print();
+    }
+    else{
+        for(int i=start; i<N; i++){
+            if(!check[i]){
+                check[i]=true;
+                ans[depth]=arr[i];
+                deep(depth+1, start+1);
+                check[i]=false;
+            }
+        }
+        
+        
+    }
+}
+
+
+int main(){
+    cin>>N>>M;
+    for(int i=0;i<N;i++){
+        arr[i]=i+1;
+    }
+    deep(0,0);
+
+    return 0;
+}
